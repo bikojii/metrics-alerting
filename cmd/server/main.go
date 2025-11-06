83ac1658f4b70e67150ab598522a20d738264322
bikojii <daracesh@gmail.com>
2025-11-06 18:24:54 +0300

    Increment 3

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"github.com/admin/metrics-alerting/internal/handler"
 	"github.com/admin/metrics-alerting/internal/repository"
+	"github.com/go-chi/chi/v5"
 	"log"
 	"net/http"
 )
 
 func main() {
 	store := repository.NewMemStorage()
+	r := chi.NewRouter()
 
-	http.HandleFunc("/update/", handler.UpdateHandler(store))
+	r.Post("/update/{type}/{name}/{value}", handler.UpdateHandler(store))
+	r.Get("/value/{type}/{name}", handler.GetValueHandler(store))
+	r.Get("/", handler.ListMetricsHandler(store))
 
 	log.Println("Server running on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
